23f4921f34ee7a96215679dd4d816af4ab6ae4e7
Rsl1122 <djrapitops@hotmail.com>
2018-03-06 21:08:52 +0200

    Remove accidental license header additions

--- src/main/java/ru/tehkode/permissions/backends/web/http/Response.java
+++ src/main/java/ru/tehkode/permissions/backends/web/http/Response.java
@@ -1,7 +1,3 @@
-/*
- * Licence is provided in the jar as license.yml also here:
- * https://github.com/Rsl1122/Plan-PlayerAnalytics/blob/master/Plan/src/main/resources/license.yml
- */
 package ru.tehkode.permissions.backends.web.http;
 
 /**
