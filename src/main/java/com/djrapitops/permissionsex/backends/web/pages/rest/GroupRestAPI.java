42608a5bb9440a5b3fe1209a95bb1f45c379f5bd
Rsl1122 <djrapitops@hotmail.com>
2018-03-08 17:50:26 +0200

    /api/groups

--- src/main/java/com/djrapitops/permissionsex/backends/web/pages/rest/GroupRestAPI.java
+++ src/main/java/com/djrapitops/permissionsex/backends/web/pages/rest/GroupRestAPI.java
@@ -1,6 +1,6 @@
 package com.djrapitops.permissionsex.backends.web.pages.rest;
 
-import com.djrapitops.permissionsex.backends.json.UserJSONService;
+import com.djrapitops.permissionsex.backends.json.GroupJSONService;
 import com.djrapitops.permissionsex.backends.web.http.Request;
 import com.djrapitops.permissionsex.backends.web.http.Response;
 import com.djrapitops.permissionsex.backends.web.http.responses.JsonErrorResponse;
@@ -11,19 +11,18 @@ import com.djrapitops.permissionsex.exceptions.web.ParseException;
 import com.google.gson.JsonArray;
 
 import java.util.List;
-import java.util.UUID;
 
 /**
  * RestAPI endpoint for /api/users.
  *
  * @author Rsl1122
  */
-public class UserRestAPI extends RestAPIHandler {
+public class GroupRestAPI extends RestAPIHandler {
 
-    private final UserJSONService userJSONService;
+    private final GroupJSONService groupJSONService;
 
-    public UserRestAPI(UserJSONService userJSONService) {
-        this.userJSONService = userJSONService;
+    public GroupRestAPI(GroupJSONService groupJSONService) {
+        this.groupJSONService = groupJSONService;
 
         registerAPIEndPoints();
     }
@@ -32,13 +31,13 @@ public class UserRestAPI extends RestAPIHandler {
         registerPage("", (request, target) -> {
             String requestMethod = request.getRequestMethod();
             if ("GET".equals(requestMethod)) {
-                // GET /api/users/ - provides all users as an array
-                return new JsonResponse(userJSONService.getAllUsers(), 200);
+                // GET /api/groups/ - provides all users as an array
+                return new JsonResponse(groupJSONService.getAllGroups(), 200);
             }
             if ("PUT".equals(requestMethod)) {
-                // PUT /api/users/ - updates users when "Save Changes" is pressed
+                // PUT /api/groups/ - updates users when "Save Changes" is pressed
                 try {
-                    userJSONService.updateUsers((JsonArray) parseJSONFromString(getStringFromRequestBody(request)));
+                    groupJSONService.updateGroups((JsonArray) parseJSONFromString(getStringFromRequestBody(request)));
                 } catch (ParseException e) {
                     return e.getCause() == null ?
                             new JsonErrorResponse(e.getMessage(), 500) :
@@ -66,13 +65,12 @@ public class UserRestAPI extends RestAPIHandler {
         }
 
         if ("GET".equals(request.getRequestMethod())) {
-            // GET /api/users/:uuid - provides a user
+            // GET /api/groups/:name - provides a group
             try {
-                String id = target.get(0);
-                UUID uuid = UUID.fromString(id);
-                return new JsonResponse(userJSONService.getUser(uuid));
+                String groupName = target.get(0);
+                return new JsonResponse(groupJSONService.getGroup(groupName));
             } catch (IllegalArgumentException e) {
-                return new JsonResponse("Invalid UUID: " + e.getMessage(), 400);
+                return new JsonResponse("Invalid Group Name: " + e.getMessage(), 400);
             }
         }
 
