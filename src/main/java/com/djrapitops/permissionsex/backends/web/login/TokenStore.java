3c94e9bc4cdf8fa4984af87811ed1e4bddf0696c
Rsl1122 <djrapitops@hotmail.com>
2018-03-08 22:21:04 +0200

    Begun work on the login system

---
+++ src/main/java/com/djrapitops/permissionsex/backends/web/login/TokenStore.java
@@ -0,0 +1,30 @@
+package com.djrapitops.permissionsex.backends.web.login;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Stores user tokens in memory.
+ *
+ * @author Rsl1122
+ */
+public class TokenStore {
+
+    private final Set<String> validTokens;
+
+    public TokenStore() {
+        validTokens = new HashSet<>();
+    }
+
+    public void addToken(String token) {
+        validTokens.add(token);
+    }
+
+    public boolean isValid(String token) {
+        return validTokens.contains(token);
+    }
+
+    public void removeToken(String token) {
+        validTokens.remove(token);
+    }
+}
