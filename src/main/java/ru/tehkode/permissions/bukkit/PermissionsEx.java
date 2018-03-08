8dabcd81fb3ba8b8e339fcb725735f2c0db3b982
AeternumStudios <aeternumstudios@gmail.com>
2018-03-08 05:56:25 -0700

    Revert "Added bstats Metrics"
    This reverts commit 915cef218dff1f71a7273ebd304ad74dbea1995b.

--- src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
+++ src/main/java/ru/tehkode/permissions/bukkit/PermissionsEx.java
@@ -24,7 +24,6 @@ import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
-import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
@@ -181,7 +180,6 @@ public class PermissionsEx extends JavaPlugin implements NativeInterface
 	@Override
 	public void onEnable()
 	{
-		Metrics metrics = new Metrics(this);
 		if(errored)
 		{
 			getLogger().severe("==== PermissionsEx could not be enabled due to an earlier error. Look at the previous server log for more info ====");
