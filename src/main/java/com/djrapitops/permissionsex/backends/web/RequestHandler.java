f68d2b46aa62bea1f73d04cd13f9f646c3fdcd42
Rsl1122 <djrapitops@hotmail.com>
2018-03-09 17:38:05 +0200

    Token Authentication

--- src/main/java/com/djrapitops/permissionsex/backends/web/RequestHandler.java
+++ src/main/java/com/djrapitops/permissionsex/backends/web/RequestHandler.java
@@ -2,13 +2,13 @@ package com.djrapitops.permissionsex.backends.web;
 
 import com.djrapitops.permissionsex.backends.web.http.Request;
 import com.djrapitops.permissionsex.backends.web.http.Response;
-import com.djrapitops.permissionsex.backends.web.http.auth.Authentication;
+import com.djrapitops.permissionsex.backends.web.http.auth.BearerTokenAuth;
+import com.djrapitops.permissionsex.backends.web.login.TokenVerifier;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Turns HttpExchange into Request that is passed onwards, and sends the Request it receives.
@@ -18,18 +18,19 @@ import java.util.List;
 public class RequestHandler implements HttpHandler {
 
 	private final ResponseHandler responseHandler;
+	private final TokenVerifier tokenVerifier;
 
 	RequestHandler(ResponseHandler responseHandler) {
 		this.responseHandler = responseHandler;
+		this.tokenVerifier = new TokenVerifier();
 	}
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		Headers requestHeaders = exchange.getRequestHeaders();
 		Headers responseHeaders = exchange.getResponseHeaders();
 
 		Request request = new Request(exchange);
-		request.setAuth(getAuthorization(requestHeaders));
+		request.setAuth(new BearerTokenAuth(tokenVerifier, request));
 
 		try {
 			Response response = responseHandler.getResponse(request);
@@ -37,16 +38,5 @@ public class RequestHandler implements HttpHandler {
 		} finally {
 			exchange.close();
 		}
-	}
-
-	private Authentication getAuthorization(Headers requestHeaders) {
-		List<String> authorization = requestHeaders.get("Authorization");
-		if (authorization == null || authorization.isEmpty()) {
-			return null;
-		}
-
-		String authLine = authorization.get(0);
-		// TODO Authentication method
-		return null;
 	}
 }
