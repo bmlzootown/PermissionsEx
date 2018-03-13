5b7c4400a39e8591c9f23f8a39e9cc61fc0c74f2
Rsl1122 <djrapitops@hotmail.com>
2018-03-13 11:15:55 +0200

    backend GET-method demo links to Frontend.

--- react/dashboard/src/App.js
+++ react/dashboard/src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+            <div>
+                <h3>Links to backend GET requests</h3>
+                <ul>
+                    <li><a href="/api/users">GET /api/users</a></li>
+                    <li><a href="/api/groups">GET /api/groups</a></li>
+                    <li><a href="/api/worlds">GET /api/worlds</a></li>
+                    <li><a href="/api/plugins">GET /api/plugins</a></li>
+                    <li><a href="/api/backups">GET /api/backups</a></li>
+                </ul>
+                <ul>
+                    <li><a href="/api/user/Player Name">GET /api/Player Name</a></li>
+                    <li><a href="/api/groups/Group Name">GET /api/groups/GroupName</a></li>
+                    <li><a href="/api/worlds/WorldName">GET /api/worlds/WorldName</a></li>
+                    <li><a href="/api/plugins/PluginName">GET /api/worlds/WorldName (Dummy returns error JSON)</a></li>
+                </ul>
             </div>
         );
     }
