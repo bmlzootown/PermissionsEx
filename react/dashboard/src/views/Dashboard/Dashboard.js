da961d3944c421ed69c90dd95580c106e0f01b10
Rsl1122 <djrapitops@hotmail.com>
2018-03-13 19:22:39 +0200

    Added CoreUI React Bootstrap template

---
+++ react/dashboard/src/views/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Dummy from '../../components/Dummy'
+
+class Dashboard extends Component {
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        <Dummy />
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
