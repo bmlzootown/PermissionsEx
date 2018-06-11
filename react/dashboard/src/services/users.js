bd046913117601549f6cca12ad2e0efbf79117dd
Rsl1122 <djrapitops@hotmail.com>
2018-06-11 12:46:34 +0300

    Error for when server is offline, proper promise exception throwing

--- react/dashboard/src/services/users.js
+++ react/dashboard/src/services/users.js
@@ -33,7 +33,7 @@ const getAll = async (token) => {
     const response = await axios.get(
         '/api/users',
         headers(token)
-    )
+    ).catch(e => { throw e })
     return response.data.list.map(mapper)
 }
 
@@ -42,7 +42,7 @@ const save = async (token, users) => {
         '/api/users',
         users.map(reverseMapper),
         headers(token)
-    )
+    ).catch(e => { throw e })
 }
 
 export default { getAll, save }
