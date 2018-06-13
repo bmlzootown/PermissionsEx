1ebd241b60c62cf0b85cae8d76f7591b04cc5c63
Rsl1122 <djrapitops@hotmail.com>
2018-06-13 10:57:25 +0300

    Added documentation and statistics links, moved logout button

--- react/dashboard/src/components/Aside/Aside.js
+++ react/dashboard/src/components/Aside/Aside.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-import LogoutButton from './LogoutButton';
+import React, { Component } from 'react'
+import LogoutButton from './LogoutButton'
+
+import { Card, CardBody } from 'reactstrap'
 
 class Aside extends Component {
-  render() {
-    return (
-      <aside className="aside-menu">
-        <LogoutButton />
-      </aside>
-    )
-  }
+    render() {
+        return (
+            <aside style={{ height: '100%' }} className="aside-menu">
+
+                <Card>
+                    <CardBody>
+                        <a style={{ width: '100%', color: '#fff' }} color="primary" className="px-4 btn btn-primary"
+                            rel="noopener noreferrer" target="_blank" href="https://pex.aeternum.network/">Documentation</a>
+                    </CardBody>
+                    <CardBody style={{ paddingTop: 0 }}>
+                        <a style={{ width: '100%', color: '#fff' }} color="primary" className="px-4 btn btn-secondary"
+                            rel="noopener noreferrer" target="_blank" href="https://bstats.org/plugin/bukkit/PermissionsEx">Statistics</a>
+                    </CardBody>
+                </Card>
+                <LogoutButton />
+            </aside>
+        )
+    }
 }
 
-export default Aside;
+export default Aside
